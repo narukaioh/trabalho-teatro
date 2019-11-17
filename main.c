80efb9ff9c0deb462ef93b4eb15b36eca16ae8f9
Ju Dantas <jdantas@thoughtworks.com>
2019-11-17 16:52:28 -0300

    Criado o menu principal do programa

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define CADASTRAR_EVENTO 1
+#define CADASTRAR_CLIENTE 2
+#define VENDER_INGRESSO 3
+
+void imprimirMenu();
+
+int main() {
+  imprimirMenu();
+  return 0;
+}
+
+void imprimirMenu() {
+  int opcao;
+  int sair = 0;
+  while(sair == 0) {
+    printf("1) Cadastrar Evento\n2) Cadastrar Cliente\n3) Vender Ingresso\n4) Sair\n");
+    scanf("%d", &opcao);
+    switch (opcao)
+    {
+    case CADASTRAR_EVENTO:
+      printf("Cadastro de Evento: \n");
+      break;
+    case CADASTRAR_CLIENTE:
+      printf("Cadastro de Cliente: \n");
+      break;
+    case VENDER_INGRESSO:
+      printf("Venda de Ingressos: \n");
+      break;
+    default:
+      sair = 1;
+      break;
+    }
+  }
+}
